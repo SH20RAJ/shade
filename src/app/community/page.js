4f6560988f90ecd5044031e309971d091ad1aed5
Sh Raj <shaswatraj3@gmail.com>
2024-03-16 05:46:34 +0000

    script removed

--- src/app/community/page.js
+++ src/app/community/page.js
@@ -11,7 +11,7 @@ export default () => {
       <Footer />
       <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-        <script src="index.js" defer></script>
+        {/* <script src="index.js" defer></script> */}
     </>
   );
 }
